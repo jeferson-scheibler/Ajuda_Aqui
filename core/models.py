d4e83ed8842d461ca9501ef4fab38a4fea8f618a
oikrad <darkioluft2008@gmail.com>
2024-06-13 19:19:43 -0300

    teste de tabela nova no models

--- core/models.py
+++ core/models.py
@@ -6,3 +6,7 @@ from django.db import models
 class CustomUser(AbstractUser):
     # Adicione campos personalizados se necessário
     pass
+class Usuarios(models.Model):
+    id_usuario = models.AutoField(primary_key=True)
+    nome = models.TextField(max_length=255)
+    idade = models.IntegerField()
