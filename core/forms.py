d7c7eb118ffa423f2109605b9bc5b45eb8563c8a
oikrad <darkioluft2008@gmail.com>
2024-06-25 18:32:40 -0300

    apresentação

--- core/forms.py
+++ core/forms.py
@@ -27,26 +27,27 @@ from .models import Task
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ('task_name', 'task_description', 'priority', 'address', 'completed', 'category', 'problem_photo', 'solution_photo')
+        fields = ('task_name', 'task_description', 'priority', 'address', 'category', 'problem_photo', 'solution_photo','completed')
         labels = {
             'task_name': _('Nome da Tarefa'),
             'task_description': _('Descrição da Tarefa'),
             'priority': _('Prioridade'),
             'address': _('Endereço'),
-            'completed': _('Tarefa Concluída'),
             'category': _('Categoria'),
             'problem_photo': _('Foto do Problema'),
             'solution_photo': _('Foto da Solução'),
+            'completed': _('Tarefa Concluída'),
         }
         widgets = {
             'task_name': forms.TextInput(attrs={'id': 'id_task_name', 'class': 'block w-full px-4 py-3 mt-2 text-gray-800 bg-white bg-opacity-90 border-2 rounded-lg border-gray-300 focus:border-yellow-500 focus:ring-opacity-50 focus:outline-none focus:ring focus:ring-yellow-400', 'placeholder': 'Nome da Tarefa'}),
             'task_description': forms.Textarea(attrs={'id': 'id_task_description', 'class': 'block w-full px-4 py-3 mt-2 text-gray-800 bg-white bg-opacity-90 border-2 rounded-lg border-gray-300 focus:border-yellow-500 focus:ring-opacity-50 focus:outline-none focus:ring focus:ring-yellow-400', 'rows': 3, 'placeholder': 'Descrição da Tarefa'}),
             'priority': forms.Select(attrs={'id': 'id_priority', 'class': 'block w-full px-4 py-3 mt-2 text-gray-800 bg-white bg-opacity-90 border-2 rounded-lg border-gray-300 focus:border-yellow-500 focus:ring-opacity-50 focus:outline-none focus:ring focus:ring-yellow-400'}),
             'address': forms.TextInput(attrs={'id': 'id_address', 'class': 'block w-full px-4 py-3 mt-2 text-gray-800 bg-white bg-opacity-90 border-2 rounded-lg border-gray-300 focus:border-yellow-500 focus:ring-opacity-50 focus:outline-none focus:ring focus:ring-yellow-400', 'placeholder': 'Endereço'}),
-            'completed': forms.CheckboxInput(attrs={'id': 'id_completed', 'class': 'form-check-input'}),
             'category': forms.Select(attrs={'id': 'id_category', 'class': 'block w-full px-4 py-3 mt-2 text-gray-800 bg-white bg-opacity-90 border-2 rounded-lg border-gray-300 focus:border-yellow-500 focus:ring-opacity-50 focus:outline-none focus:ring focus:ring-yellow-400'}),
             'problem_photo': forms.ClearableFileInput(attrs={'id': 'id_problem_photo', 'class': 'flex w-full rounded-md border border-yellow-300 border-input bg-white text-sm text-gray-400 file:border-0 file:bg-yellow-400 file:text-white file:text-sm file:font-medium '}),
             'solution_photo': forms.ClearableFileInput(attrs={'id': 'id_solution_photo', 'class': 'flex w-full rounded-md border border-yellow-300 border-input bg-white text-sm text-gray-400 file:border-0 file:bg-yellow-400 file:text-white file:text-sm file:font-medium'}),
+            'completed': forms.CheckboxInput(attrs={'id': 'id_completed', 'class': 'px-4 py-2 bg-blue-500 text-white rounded cursor-pointer select-none'}),
+
         }
 
     def clean_priority(self):
